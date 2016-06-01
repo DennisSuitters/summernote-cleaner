72835928f2ab771c73caf3d6ede6623607ea337d
wheelspin <banish.haus@gmail.com>
2016-06-01 11:54:39 -0500

    Edge Clipboard, alert div to class
    I ran into a pasting issue for Microsoft Edge which probably affects other versions of Internet Explorer. Because of a security setting, IE may block access to the clipboard http://www.thewindowsclub.com/harden-clipboard-data-theft-ie. There is certainly a cleaner way to write it but the code.
    Also for reference: https://msdn.microsoft.com/en-us/library/ms535220(v=vs.85).aspx
    
    There was also an issue where two summernote divs with separate ids are created and after a paste event, the cleanerAlert div was added and then only removed from the first of the two editors.  By switching to class, the timer removes it from both.

--- summernote-cleaner.js
+++ summernote-cleaner.js
@@ -73,12 +73,19 @@
                 'summernote.paste':function(we,e){
                     if(options.action=='both'||options.action=='paste'){
                         e.preventDefault();
-                        var text=e.originalEvent.clipboardData.getData('text/plain');
+                        var ua = window.navigator.userAgent;
+                        var msie = ua.indexOf("MSIE ");
+                        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  { // If Internet Explorer, return version number
+                            var text=window.clipboardData.getData("Text");
+                            
+                        } else { // If another browser, return 0
+                            var text=e.originalEvent.clipboardData.getData('text/plain');
+                        }
                         var text=cleanText(text);
                         var $dom=$('<div class="pasted"/>').html(text);
                         $note.summernote('insertNode',$dom[0]);
-                        $('.note-resizebar').append('<div id="cleanerAlert" style="position:absolute;bottom:0;left:2px;font-size:10px;">'+lang.cleaner.notification+'</div>');
-                        setTimeout(function(){$('#cleanerAlert').remove();},options.time);
+                        $('.note-resizebar').append('<div class="cleanerAlert" style="position:absolute;bottom:0;left:2px;font-size:10px;">'+lang.cleaner.notification+'</div>');
+                        setTimeout(function(){$('.cleanerAlert').remove();},options.time);
                     }
                 }
             }
