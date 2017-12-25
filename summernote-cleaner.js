8ced7d1fae1c029b4b7967b3d723fe31dfae2fc6
root <dennis@studiojunkyard.com>
2017-12-25 17:33:34 +1100

    Fix formatting.

--- summernote-cleaner.js
+++ summernote-cleaner.js
@@ -8,12 +8,14 @@
     factory(window.jQuery);
   }
 }
-(function($) {
-  $.extend(true,$.summernote.lang, {
+(function ($) {
+  $.extend(true, $.summernote.lang, {
     'en-US': {
       cleaner: {
         tooltip: 'Cleaner',
-        not: 'Text has been Cleaned!!!'
+        not: 'Text has been Cleaned!!!',
+        limitText: 'Text',
+        limitHTML: 'HTML'
       }
     }
   });
@@ -28,20 +30,20 @@
       keepClasses: false, //Remove Classes
       badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'html'], //Remove full tags with contents
       badAttributes: ['style', 'start'], //Remove attributes from remaining tags
-      limitChars: 0, // 0|# 0 disables option
+      limitChars: 520, // 0|# 0 disables option
       limitDisplay: 'both', // none|text|html|both
       limitStop: false // true/false
     }
   });
   $.extend($.summernote.plugins, {
-    'cleaner':function(context) {
-      var self = this;
-      var ui = $.summernote.ui;
-      var $note = context.layoutInfo.note;
-      var $editor = context.layoutInfo.editor;
-      var options = context.options;
-      var lang = options.langInfo;
-      var cleanText = function(txt, nlO) {
+    'cleaner':function (context) {
+      var self = this,
+            ui = $.summernote.ui,
+         $note = context.layoutInfo.note,
+       $editor = context.layoutInfo.editor,
+       options = context.options,
+          lang = options.langInfo;
+      var cleanText = function (txt, nlO) {
         var out = txt;
         if (!options.cleaner.keepClasses) {
           var sS = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
@@ -57,22 +59,21 @@
           var bT = options.cleaner.badTags;
           for (var i = 0; i < bT.length; i++) {
             // suggestion by @LoloDf at #40
-            tS=new RegExp('<'+bT[i]+'\\b.*>.*</'+bT[i]+'>','gi');
+            tS=new RegExp('<' + bT[i] + '\\b.*>.*</' + bT[i] + '>', 'gi');
             out = out.replace(tS, '');
           }
           var allowedTags = options.cleaner.keepOnlyTags;
-          if (typeof(allowedTags) == "undefined")
-            allowedTags = [];
+          if (typeof(allowedTags) == "undefined") allowedTags = [];
           if (allowedTags.length > 0) {
             allowedTags = (((allowedTags||'') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
             var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
-            out = out.replace(tags,function($0, $1) {
-              return allowedTags.indexOf('<' + $1.toLowerCase()+'>') > -1 ? $0 : ''
+            out = out.replace(tags, function($0, $1) {
+              return allowedTags.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
             });
           }
           var bA = options.cleaner.badAttributes;
           for (var ii = 0; ii < bA.length; ii++ ) {
-            //var aS=new RegExp(' ('+bA[ii]+'="(.*?)")|('+bA[ii]+'=\'(.*?)\')','gi');
+            //var aS=new RegExp(' ('+bA[ii]+'="(.*?)")|('+bA[ii]+'=\'(.*?)\')', 'gi');
             var aS = new RegExp(' ' + bA[ii] + '=[\'|"](.*?)[\'|"]', 'gi');
             out = out.replace(aS, '');
           }
@@ -80,11 +81,11 @@
         return out;
       };
       if (options.cleaner.action == 'both' || options.cleaner.action == 'button') {
-        context.memo('button.cleaner',function() {
+        context.memo('button.cleaner', function () {
           var button = ui.button({
             contents: options.cleaner.icon,
             tooltip: lang.cleaner.tooltip,
-            click:function() {
+            click:function () {
               if ($note.summernote('createRange').toString()) {
                 var text = $note.summernote('createRange').toString();
                 $note.summernote('pasteHTML', text);
@@ -92,42 +93,45 @@
                 var text = cleanText($note.summernote('code'));
                 $note.summernote('code', text);
               }
-                if($('.note-status-output').length > 0){
-                    $('.note-status-output').html('<div class="summernote-cleanerAlert alert alert-success">' + lang.cleaner.not + '</div>');
-                } else {
-                  $editor.find('.note-editing-area').append('<div class="summernote-cleanerAlert alert alert-success" style="' + options.cleaner.notStyle + '">' + lang.cleaner.not + '</div>');
-                }
+              if ($('.note-status-output').length > 0) {
+                $('.note-status-output').html('<div class="alert alert-success">' + lang.cleaner.not + '</div>');
+              } else {
+                $editor.find('.note-editing-area').append('<div class="alert alert-success" style="' + options.cleaner.notStyle + '">' + lang.cleaner.not + '</div>');
+              }
             }
           });
           return button.render();
         });
       }
       this.events = {
-        'summernote.init': function () {
-          if($('.note-status-output').length < 1) {
+        'summernote.init':function () {
+          if ($('.note-status-output').length < 1) {
             $('.note-status-bar').prepend('<output class="note-status-output"></output>');
             $("head").append('<style>.note-editor.note-frame .note-statusbar .note-status-output{font-size:12px;height:25px}.note-editor.note-frame .note-statusbar .note-status-output .alert{margin:-7px 0 0 0;padding:2px 10px;border:1px solid transparent;border-radius:0}.note-editor.note-frame .note-statusbar .note-status-output .alert .note-icon{margin-right:5px}.note-editor.note-frame .note-statusbar .note-status-output .alert .alert-success{color:#3c763d;background-color:#dff0d8;border-color:#d6e9c6}.note-editor.note-frame .note-statusbar .note-status-output .alert .alert-info{color:#31708f;background-color:#d9edf7;border-color:#bce8f1}.note-editor.note-frame .note-statusbar .note-status-output .alert .alert-warning{color:#8a6d3b;background-color:#fcf8e3;border-color:#faebcc}.note-editor.note-frame .note-statusbar .note-status-output .alert .alert-danger{color:#a94442;background-color:#f2dede;border-color:#ebccd1}</style>');
           }
-          if(options.cleaner.limitChars != 0 || options.cleaner.limitDisplay != 'none') {
+          if (options.cleaner.limitChars != 0 || options.cleaner.limitDisplay != 'none') {
             var textLength = $(".note-editable").text().replace(/(<([^>]+)>)/ig, "").replace(/( )/, " ");
             var codeLength = $('.note-editable').html();
             var lengthStatus = '';
-            if (textLength.length > options.cleaner.limitChars && options.cleaner.limitChars > 0) lengthStatus += 'text-danger">'; else lengthStatus += 'text-muted">';
-            if (options.cleaner.limitDisplay == 'text' || options.cleaner.limitDisplay == 'both') lengthStatus += 'Text: ' + textLength.length;
+            if (textLength.length > options.cleaner.limitChars && options.cleaner.limitChars > 0)
+              lengthStatus += 'text-danger">';
+            else
+              lengthStatus += 'text-muted">';
+            if (options.cleaner.limitDisplay == 'text' || options.cleaner.limitDisplay == 'both') lengthStatus += lang.cleaner.limitText + ': ' + textLength.length;
             if (options.cleaner.limitDisplay == 'both') lengthStatus += ' / ';
-            if (options.cleaner.limitDisplay == 'html' || options.cleaner.limitDisplay == 'both') lengthStatus += 'HTML: ' + codeLength.length;
+            if (options.cleaner.limitDisplay == 'html' || options.cleaner.limitDisplay == 'both') lengthStatus += lang.cleaner.limitHTML + ': ' + codeLength.length;
             $('.note-status-output').html('<small class="pull-right ' + lengthStatus + '&nbsp;</small>');
           }
         },
-        'summernote.keydown': function (we,e) {
-          if(options.cleaner.limitChars != 0 || options.cleaner.limitDisplay != 'none') {
-            var textLength = $(".note-editable").text();
+        'summernote.keydown':function (we, e) {
+          if (options.cleaner.limitChars != 0 || options.cleaner.limitDisplay != 'none') {
+            var textLength = $(".note-editable").text().replace(/(<([^>]+)>)/ig, "").replace(/( )/, " ");
             var codeLength = $('.note-editable').html();
             var lengthStatus = '';
-            if(options.cleaner.limitStop == true && textLength.length >= options.cleaner.limitChars) {
+            if (options.cleaner.limitStop == true && textLength.length >= options.cleaner.limitChars) {
               var key = e.keyCode;
               allowed_keys = [8, 37, 38, 39, 40, 46]
-              if($.inArray(key, allowed_keys) != -1) {
+              if ($.inArray(key, allowed_keys) != -1) {
                 $('.cleanerLimit').removeClass('text-danger');
                 return true;
               } else {
@@ -136,15 +140,18 @@
                 e.stopPropagation();
               }
             } else {
-              if (textLength.length > options.cleaner.limitChars && options.cleaner.limitChars > 0) lengthStatus += 'text-danger">'; else lengthStatus += 'text-muted">';
-              if (options.cleaner.limitDisplay == 'text' || options.cleaner.limitDisplay == 'both') lengthStatus += 'Text: ' + textLength.length;
+              if (textLength.length > options.cleaner.limitChars && options.cleaner.limitChars > 0)
+                lengthStatus += 'text-danger">';
+              else
+                lengthStatus += 'text-muted">';
+              if (options.cleaner.limitDisplay == 'text' || options.cleaner.limitDisplay == 'both') lengthStatus += lang.cleaner.limitText + ': ' + textLength.length;
               if (options.cleaner.limitDisplay == 'both') lengthStatus += ' / ';
-              if (options.cleaner.limitDisplay == 'html' || options.cleaner.limitDisplay == 'both') lengthStatus += 'HTML: ' + codeLength.length;
+              if (options.cleaner.limitDisplay == 'html' || options.cleaner.limitDisplay == 'both') lengthStatus += lang.cleaner.limitHTML + ': ' + codeLength.length;
               $('.note-status-output').html('<small class="cleanerLimit pull-right ' + lengthStatus + '&nbsp;</small>');
             }
           }
         },
-        'summernote.paste': function (we, e) {
+        'summernote.paste':function (we, e) {
           if (options.cleaner.action=='both' || options.cleaner.action=='paste') {
             var ua = window.navigator.userAgent;
             var msie = ua.indexOf("MSIE ");
@@ -155,10 +162,10 @@
             } else {
               var text = e.originalEvent.clipboardData.getData((options.cleaner.keepHtml ? 'text/html' : 'text/plain'));
             }
-            if (text && text!='') {
+            if (text && text != '') {
               e.preventDefault();
               var text=cleanText(text, options.cleaner.newline);
-              var pasteFn = function() {
+              var pasteFn = function () {
                 $note.summernote('pasteHTML', text);
               }
               if (msie || ffox) {
@@ -166,11 +173,11 @@
               } else {
                 pasteFn();
               }
-                if($('.note-status-output').length > 0){
-                    $('.note-status-output').html('<div class="summernote-cleanerAlert alert alert-success">' + lang.cleaner.not + '</div>');
-                } else {
-                  $editor.find('.note-resizebar').append('<div class="summernote-cleanerAlert alert alert-success" style="' + options.cleaner.notStyle + '">' + lang.cleaner.not + '</div>');
-                }
+              if ($('.note-status-output').length > 0) {
+                $('.note-status-output').html('<div class="summernote-cleanerAlert alert alert-success">' + lang.cleaner.not + '</div>');
+              } else {
+                $editor.find('.note-resizebar').append('<div class="summernote-cleanerAlert alert alert-success" style="' + options.cleaner.notStyle + '">' + lang.cleaner.not + '</div>');
+              }
             }
           }
         }
