9627386e2ffae5aef319e5403ee482e069ef9167
Richard Squires <richard.squires@sorce.co.uk>
2022-03-03 10:53:47 +0000

    Fixed issue that prevent pasting of plain text

--- summernote-cleaner.js
+++ summernote-cleaner.js
@@ -1,5 +1,5 @@
 /* https://github.com/DiemenDesign/summernote-cleaner */
-/* Version: 1.0.1 */
+/* Version: 1.0.2 */
 (function(factory) {
   if (typeof define === 'function' && define.amd) {
     define(['jquery'], factory);
@@ -31,7 +31,6 @@
   $.extend( $.summernote.options, {
     cleaner: {
       action: 'both', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
-      newline: '<br>', // Summernote's default is to use '<p><br></p>'
       icon: '<i class="note-icon"><svg xmlns="http://www.w3.org/2000/svg" id="libre-paintbrush" viewBox="0 0 14 14" width="14" height="14"><path d="m 11.821425,1 q 0.46875,0 0.82031,0.311384 0.35157,0.311384 0.35157,0.780134 0,0.421875 -0.30134,1.01116 -2.22322,4.212054 -3.11384,5.035715 -0.64956,0.609375 -1.45982,0.609375 -0.84375,0 -1.44978,-0.61942 -0.60603,-0.61942 -0.60603,-1.469866 0,-0.857143 0.61608,-1.419643 l 4.27232,-3.877232 Q 11.345985,1 11.821425,1 z m -6.08705,6.924107 q 0.26116,0.508928 0.71317,0.870536 0.45201,0.361607 1.00781,0.508928 l 0.007,0.475447 q 0.0268,1.426339 -0.86719,2.32366 Q 5.700895,13 4.261155,13 q -0.82366,0 -1.45982,-0.311384 -0.63616,-0.311384 -1.0212,-0.853795 -0.38505,-0.54241 -0.57924,-1.225446 -0.1942,-0.683036 -0.1942,-1.473214 0.0469,0.03348 0.27455,0.200893 0.22768,0.16741 0.41518,0.29799 0.1875,0.130581 0.39509,0.24442 0.20759,0.113839 0.30804,0.113839 0.27455,0 0.3683,-0.247767 0.16741,-0.441965 0.38505,-0.753349 0.21763,-0.311383 0.4654,-0.508928 0.24776,-0.197545 0.58928,-0.31808 0.34152,-0.120536 0.68974,-0.170759 0.34821,-0.05022 0.83705,-0.07031 z"/></svg></i>',
       keepHtml: true,
       badTags: ['applet', 'col', 'colgroup', 'embed', 'noframes', 'noscript', 'script', 'style', 'title'], //Remove full tags with contents
@@ -61,7 +60,7 @@
               if ($note.summernote('createRange').toString())
                 $note.summernote('pasteHTML', $note.summernote('createRange').toString());
               else
-                $note.summernote('code', cleanPaste($note.summernote('code'), options.cleaner.badTags, options.cleaner.badAttributes));
+                $note.summernote('code', cleanPaste($note.summernote('code'), options.cleaner.badTags, options.cleaner.badAttributes), true);
               if ($editor.find('.note-status-output').length > 0)
                 $editor.find('.note-status-output').html(lang.cleaner.not);
             }
@@ -126,24 +125,57 @@
             var msie = ua.indexOf("MSIE ");
                 msie = msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./);
             var ffox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+            var text; var isHtmlData = false;
             if (msie)
-              var text = window.clipboardData.getData("Text");
+              text = window.clipboardData.getData("Text");
             else
-              var text = event.originalEvent.clipboardData.getData(options.cleaner.keepHtml ? 'text/html' : 'text/plain');
+            {
+              var dataType = 'text/plain';
+              /*only get the html data if its avaialble else use plain text*/
+              if (options.cleaner.keepHtml && event.originalEvent.clipboardData.types.indexOf('text/html') > -1) {
+                  dataType = 'text/html';
+                  isHtmlData = false;
+              }
+              text = event.originalEvent.clipboardData.getData(dataType);
+            }
             if (text) {
               if (msie || ffox) {
                 setTimeout(function () {
-                  $note.summernote('pasteHTML', cleanPaste(text, options.cleaner.badTags, options.cleaner.badAttributes));
+                  $note.summernote('pasteHTML', cleanPaste(text, options.cleaner.badTags, options.cleaner.badAttributes, isHtmlData));
                 }, 1);
               } else
-                $note.summernote('pasteHTML', cleanPaste(text, options.cleaner.badTags, options.cleaner.badAttributes));
+                $note.summernote('pasteHTML', cleanPaste(text, options.cleaner.badTags, options.cleaner.badAttributes, isHtmlData));
               if ($editor.find('.note-status-output').length > 0)
                 $editor.find('.note-status-output').html(lang.cleaner.not);
             }
           }
         }
       }
-      var cleanPaste = function(input, badTags, badAttributes) {
+      var cleanPaste = function(input, badTags, badAttributes, isHtmlData) {
+        if(isHtmlData) {
+          return cleanHtmlPaste(input, badTags, badAttributes);
+        } else {
+          return cleanTextPaste(input);
+        }
+      };
+
+      var cleanTextPaste = function(input) {
+        var newLines = /(\r\n|\r|\n)/g;
+        var parsedInput = input.split(newLines);
+        if(parsedInput.length === 1) { return input; }
+        var output = "";
+        /*for larger blocks of text (such as multiple paragraphs) match summernote markup */
+        for (let contentIndex = 0; contentIndex < parsedInput.length; contentIndex++) {
+          const element = parsedInput[contentIndex];
+          if(!newLines.test(element)) {
+            var line = element == '' ? '<br>' : element;
+            output += '<p>' + line + '</p>'
+          }
+        }
+        return output;
+      }
+      
+      var cleanHtmlPaste = function(input, badTags, badAttributes) {
         var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")? ^p)/g;
         var output = input.replace(stringStripper, '');
         var commentSripper = new RegExp('<!--(.*?)-->', 'g');
